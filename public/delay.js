dd9ec2e8e16640d488fbcfd0428f021d92f72d04
Élő Zalán <90603993+EloZalan@users.noreply.github.com>
2025-03-26 17:27:31 +0100

    kb kész

---
+++ public/delay.js
@@ -0,0 +1,58 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let con = false;
+let AllPlanesDelay = [];
+function fetchPlaneDelay() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("http://localhost:3000/userFlights");
+        if (!response.ok) {
+            throw new Error("Failed to fetch planes");
+        }
+        const data = yield response.json();
+        return data;
+    });
+}
+;
+function displayPlaneDelay() {
+    return __awaiter(this, arguments, void 0, function* (type = 'all') {
+        const plane = yield fetchPlaneDelay();
+        const currentDate = getCurrentDateFormatted();
+        AllPlanesDelay = plane.filter(x => x.Departure_Date == currentDate);
+        let delayTable = document.getElementById('delayTableDiv');
+        console.log(AllPlanesDelay);
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <th>Indulási idő</th>
+            <th>Késés (perc)</th>
+            <th>Reptér</th>
+            <th>Repülési kód</th>
+        `;
+        delayTable.appendChild(tr);
+        AllPlanesDelay.forEach(element => {
+            const tr = document.createElement('tr');
+            tr.innerHTML =
+                `
+                        <td>${element.Departure_Time}</td>
+                        <td class="text-danger">${Math.floor(Math.random() * 20) >= 5 ? Math.floor(Math.random() * 50) : ""}</td>
+                        <td>${element.Airport_From}</td>
+                        <td>${element.Flight_Number}</td>
+                        <hr>
+            `;
+            delayTable.appendChild(tr);
+        });
+    });
+}
+;
+displayPlaneDelay();
+function getCurrentDateFormatted() {
+    const date = new Date();
+    return date.toISOString().split('T')[0];
+}
---
+++ src/delay.ts
@@ -0,0 +1,78 @@
+interface Plane{
+    id: number,
+    Departure_Date: string,
+    Departure_Time: string,
+    Destination_Date: string,
+    Destination_Time: string,
+    Airport_From: string,
+    Airport_To: string,
+    Price: number,
+    Plane_Type: string,
+    Free_Seats: number,
+    Flight_Number: string,
+}
+
+let con = false;
+let AllPlanesDelay: Plane[] = []
+
+async function fetchPlaneDelay() : Promise<Plane[]> {
+    const response =  await fetch("http://localhost:3000/userFlights");
+    if (!response.ok) {
+        throw new Error("Failed to fetch planes");
+    }
+    const data = await response.json();
+    return data;
+};
+
+async function displayPlaneDelay(type: string = 'all') {
+    const plane = await fetchPlaneDelay();
+    const currentDate = getCurrentDateFormatted();
+    AllPlanesDelay = plane.filter(x => x.Departure_Date == currentDate);
+    let delayTable = document.getElementById('delayTableDiv') as HTMLDivElement
+    console.log(AllPlanesDelay);
+    
+      
+    const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <th>Indulási idő</th>
+            <th>Késés (perc)</th>
+            <th>Reptér</th>
+            <th>Repülési kód</th>
+        `;
+        delayTable.appendChild(tr);
+        AllPlanesDelay.forEach(element => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = 
+            `
+                        <td>${element.Departure_Time}</td>
+                        <td class="text-danger">${Math.floor(Math.random()* 20) >= 5 ? Math.floor(Math.random()* 50) : "" }</td>
+                        <td>${element.Airport_From}</td>
+                        <td>${element.Flight_Number}</td>
+                        <hr>
+            `
+            delayTable.appendChild(tr);
+        });
+
+    
+      
+   
+    
+   
+};
+
+
+displayPlaneDelay()
+
+    
+function getCurrentDateFormatted(): string {
+    const date = new Date();
+    return date.toISOString().split('T')[0];
+}
+
+
+
+
+
+
+
+
